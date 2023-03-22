e95e07351c4e16d48e53716a3461272d0ef6db30
felipemth92 <felipematheus92@gmail.com>
2023-03-22 14:57:17 -0300

    create simple bucket

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+    type = string
+    description = "The Google Cloud Project ID"
+}
+
+variable "region" {
+    type = string
+    description = "Google Cloud location of resources"
+}
