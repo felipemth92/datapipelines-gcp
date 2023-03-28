fbe9069849c3ba27371e6178af1f3d21543380aa
felipemth92 <felipematheus92@gmail.com>
2023-03-28 16:17:14 -0300

    create dataset, table and buckets v2

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "bigquery-dataset-gasolina" {
         expiration_time = null,
         clustering      = ["produto","regiao_sigla", "estado_sigla"],
         labels          = {
-          name    = "datapipeline01"
+          name    = "datapipeline02"
           project  = "gasolina"
         },
         deletion_protection = true
